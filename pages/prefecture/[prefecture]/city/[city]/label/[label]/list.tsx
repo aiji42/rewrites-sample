92551776f56260466c9b77550ace6c9cd7e8d000
Aiji Uejima <aiji42@gmail.com>
2021-05-18 00:00:12 +0900

    importのエイリアス

--- pages/prefecture/[prefecture]/city/[city]/label/[label]/list.tsx
+++ pages/prefecture/[prefecture]/city/[city]/label/[label]/list.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../../../../../../../styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 import { GetServerSideProps } from 'next'
 
 interface PageProps {
